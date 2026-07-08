f14a1efc5a5439cfd1a3080a69a4bcc56978c230
rajkonkret <rajkonkret660@gmail.com>
2026-07-08 14:31:53 +0200

    [1 27] add formaty.py

---
+++ dzien1/typy_danych_1.py
@@ -0,0 +1,17 @@
+wiek = 47  # int
+rok = 2026  # int
+temp = 36.6  # float
+
+print(wiek + rok)  # 2073
+print(wiek - rok)  # -1979
+print(wiek * rok)  # 95222
+print(wiek / rok)  # 0.02319842053307009 -> float
+
+print(wiek / 1)  # 47.0
+
+print(2026 / 47)  # 43.1063829787234
+print(2026 // 47)  # 43, częśc całkowita dzielnia
+
+print(2026 - (2026 // 47) * 47)  # reszta z dzielenia 5
+
+print(rok % wiek)  # modulo, reszta z dzielenia 5
