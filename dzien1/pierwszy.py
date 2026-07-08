b7ac0ba44808e980f8de1cbd8751c08a80cafc46
rajkonkret <rajkonkret660@gmail.com>
2026-07-08 12:01:28 +0200

    [1 4] add pierwszy.py

---
+++ dzien1/pierwszy.py
@@ -0,0 +1,20 @@
+# pep8
+
+print('Hello World')
+print()
+print()
+
+print("Nazywam się Radek")  # Nazywam się Radek
+print('Nazywam się Radek')
+
+# ctrl / - komentarz
+# print('Radek")
+#   File "C:\Users\CSComarch\PycharmProjects\PythonPodstawy-8-07-2026\dzien1\pierwszy.py", line 10
+#     print('Radek")
+#           ^
+# SyntaxError: unterminated string literal (detected at line 10)
+#
+# Process finished with exit code 1
+
+print("Dalszy ciąg programu")  # Dalszy ciąg programu
+# ctrl alt l - formatowanie
