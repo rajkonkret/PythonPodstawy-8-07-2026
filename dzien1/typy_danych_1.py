aad9865b8bcc9fa5d4077bd7705c9559c978ab66
rajkonkret <rajkonkret660@gmail.com>
2026-07-08 14:52:46 +0200

    [1 32] add typy_danych_1.py

--- dzien1/typy_danych_1.py
+++ dzien1/typy_danych_1.py
@@ -39,3 +39,30 @@ print(0.2 + 0.7)  # 0.8999999999999999
 print(sys.float_info)
 # sys.float_info(max=1.7976931348623157e+308, max_exp=1024, max_10_exp=308, min=2.2250738585072014e-308, min_exp=-1021,
 # min_10_exp=-307, dig=15, mant_dig=53, epsilon=2.220446049250313e-16, radix=2, rounds=1)
+
+# typ logiczny
+# prawda, fałsz
+# True, False
+# 1, 0
+
+czy_znasz_pythona = True
+print(czy_znasz_pythona)  # True
+print(type(czy_znasz_pythona))  # <class 'bool'>
+
+print(int(True))  # 1
+print(int(False))  # 0
+
+# bool() - rzutowanie na typ logiczny
+print(bool(1))  # True
+print(bool(0))  # False
+
+print(bool(100))  # True
+print(bool(200))  # True
+print(bool(-200))  # True
+
+print(bool(""))  # False
+
+print(bool(" "))  # "True"
+print(bool("Rdek"))  # "True"
+
+print(bool("0"))  # True
