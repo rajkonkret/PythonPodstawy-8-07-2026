b743574638113d175fb0a040ddf2d1b7eba8a997
rajkonkret <rajkonkret660@gmail.com>
2026-07-08 15:36:55 +0200

    [1 41] add typy_danych_1.py

--- dzien1/typy_danych_2_lista.py
+++ dzien1/typy_danych_2_lista.py
@@ -63,3 +63,19 @@ print(lista[:])
 # ['Radek', 'Tomek', 'Marek', 'Zenek', 'Anna', 'Karolina']
 #     0        1        2        3        4         5
 #     -6       -5       -4       -3      -2        -1
+
+print(lista[-2:0])  # [] [4:0]
+print(lista[0:-2])  # [0:4],['Radek', 'Tomek', 'Marek', 'Zenek']
+
+# od 0 do 14
+# range()
+lista_15 = list(range(15))  # od 0 do 14
+print(lista_15)  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
+print(lista_15[::2])  # [start:stop:krok], [0, 2, 4, 6, 8, 10, 12, 14]
+print(lista_15[::3])  # [0, 3, 6, 9, 12]
+
+print(lista_15[::-1])
+# [14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+
+print(list(range(1, 15, 2)))  # [1, 3, 5, 7, 9, 11, 13]
