b93f94d8690a8e4fa602e12b52b5f23ca89bb110
rajkonkret <rajkonkret660@gmail.com>
2026-07-08 14:10:56 +0200

    [1 22] add formaty.py

---
+++ dzien1/formaty.py
@@ -0,0 +1,29 @@
+user = "Tomek"  # str
+wiek = 39  # int
+
+wersja = 3.90001
+print(type(wersja))  # <class 'float'>, zmiennoprzecinkowe
+
+liczba = 9087654567891234321  # int
+
+print("Witaj %s, masz teraz %d lat." % (user, wiek))
+# Witaj Tomek, masz teraz 39 lat.
+# %s - str
+# %d - digit (liczba)
+
+# print("Witaj %d, masz teraz %s lat." % (user, wiek))
+#
+# TypeError: %d format: a real number is required, not str
+
+
+print(f"Witaj {user}, masz teraz {wiek} lat.")
+# Witaj Tomek, masz teraz 39 lat.
+
+print("Używamy wersji Pythona %i" % 3)  # Używamy wersji Pythona 3
+print("Używamy wersji Pythona %f" % 3)  # Używamy wersji Pythona 3.000000
+print("Używamy wersji Pythona %.2f" % 3)  # Używamy wersji Pythona 3.00
+print("Używamy wersji Pythona %.2f" % 3.9)  # Używamy wersji Pythona 3.90
+print("Używamy wersji Pythona %.1f" % 3.9)  # Używamy wersji Pythona 3.9
+print("Używamy wersji Pythona %.1f" % 3.99)  # Używamy wersji Pythona 4.0
+print("Używamy wersji Pythona %.0f" % 3.9)  # Używamy wersji Pythona 4 zaokrągli wyświetlanie
+print("Używamy wersji Pythona %.f" % 3.9)  # Używamy wersji Pythona 4 zaokrągli wyświetlanie
