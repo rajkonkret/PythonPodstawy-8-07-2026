f4bdd46097fe1b44c8ed9dbd327cf9cc15949b74
rajkonkret <rajkonkret660@gmail.com>
2026-07-08 15:18:06 +0200

    [1 35] add typy_danych_1.py

---
+++ dzien1/typy_danych_2_lista.py
@@ -0,0 +1,24 @@
+# kolekcja
+
+# lista - przechowuje dowolna ilosc danych, róznego typu na raz
+# zachowuje kolejnosc przy dodawaniu elementów
+
+# pusta lista
+lista = []
+print(lista)  # []
+print(type(lista))  # <class 'list'>
+
+pusta_lista = list()
+print(type(pusta_lista))  # <class 'list'>
+print(pusta_lista)  # []
+
+# dodawanie elemntów do listy
+lista.append("Radek")
+lista.append("Tomek")
+lista.append("Marek")
+lista.append("Zenek")
+lista.append("Anna")
+lista.append("Karolina")
+print(lista)
+# ['Radek', 'Tomek', 'Marek', 'Zenek', 'Anna', 'Karolina']
+
