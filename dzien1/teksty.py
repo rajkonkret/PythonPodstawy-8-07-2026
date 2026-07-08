f0ef5b6b6ba7804687e2a7193eacf83ad0352998
rajkonkret <rajkonkret660@gmail.com>
2026-07-08 12:56:32 +0200

    [1 11] add teksty.py

--- dzien1/teksty.py
+++ dzien1/teksty.py
@@ -19,3 +19,16 @@ print(tekst.lower())  # witaj świecie
 print(tekst.capitalize())  # Witaj świecie
 
 # title()
+
+# Witaj Świecie
+# 0123456789....
+
+print(tekst[1])  # i
+print(tekst[3])  # a
+print(tekst[6])  # Ś
+
+print(tekst.index("Ś"))  # indeks numer 6
+print(tekst.index("e"))  # index 9, pierwszy z lewej
+
+print(tekst.count("w"))  # występuje 1 raz
+print(tekst.lower().count("w"))  # występuje 2 razy
