cca6e0dcebd0867fff6c47e3c1b82c6cfbc8022a
rajkonkret <rajkonkret660@gmail.com>
2026-07-09 11:20:53 +0200

    [2 20] add typy_danych_4_zbior.py

---
+++ dzien2/typy_danych_5_slownik.py
@@ -0,0 +1,14 @@
+# słownik - para klucz : wartosc
+# {"user" : "Radek"}
+# klucze nie mogą sie powtarzac
+# {"name":"John", "age":30, "car":null}
+# słownik jest odpowiednikiem jsona
+
+# pusty słownik
+dictionary = {}
+print(dictionary)  # {}
+print(type(dictionary))  # <class 'dict'>
+
+dictionary_1 = dict()
+print(dictionary_1)  # {}
+print(type(dictionary_1))  # <class 'dict'>
