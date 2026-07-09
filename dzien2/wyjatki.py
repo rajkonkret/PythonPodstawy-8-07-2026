e474a93cf4e1b550240d21e9aabd4bf987b921ae
rajkonkret <rajkonkret660@gmail.com>
2026-07-09 15:21:42 +0200

    [2 58] add warunki2.py

--- dzien2/wyjatki.py
+++ dzien2/wyjatki.py
@@ -12,12 +12,29 @@
 
 try:
     # print(5 / 0)
-    int('A')  # ValueError: invalid literal for int() with base 10: 'A'
+    # int('A')  # ValueError: invalid literal for int() with base 10: 'A'
+    # print(2 + "A")  # TypeError: unsupported operand type(s) for +: 'int' and 'str'
+    # raise KeyError("Bład klucza")
+    wynik = 20 / 3
 except ZeroDivisionError:
     print("Nie dziel przez zero")
+except ValueError:
+    print("Bład wartości")
+except TypeError:
+    print("Bład typu")
+except Exception as e:
+    print("Bład:", e)
+else:  # wykona się tylko gdy nie ma błedu
+    print("wynik:", wynik)
+finally:  # wykona się zawsze
+    print("kolejne obliczenie")
+    print(50 * "-")
 
+# try, except [else, finally]
 print("Program działa nadal")
 # Nie dziel przez zero
 # Program działa nadal
 #
 # Process finished with exit code 0
+# Bład: 'Bład klucza'
+# Program działa nadal
