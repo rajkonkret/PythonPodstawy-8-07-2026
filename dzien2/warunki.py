7171ea5e3cdd6efb889edcdc75f710e7465423b2
rajkonkret <rajkonkret660@gmail.com>
2026-07-09 14:10:50 +0200

    [2 46] add warunki.py

--- dzien2/warunki.py
+++ dzien2/warunki.py
@@ -75,6 +75,10 @@ podatek = 0
 # kolejnosc ma znaczenie
 if zarobki < 10_000:
     podatek = 0
+# elif zarobki >= 10_000 and zarobki < 40_000:
+# elif 10_000 <= zarobki < 40_000:
+elif zarobki < 40_000:
+    podatek = 0.2
 elif zarobki < 100_000:
     podatek = 0.4
 else:
@@ -83,4 +87,6 @@ else:
 print(f"Podatek wynosi: {zarobki * podatek} pln.")
 # Podaj zrobki: 15000
 # Podatek wynosi: 6000.0 pln.
-#
+# podatek 0.2 dla dochodu od 10000 do 39999
+# Podaj zrobki: 78000
+# Podatek wynosi: 31200.0 pln.
