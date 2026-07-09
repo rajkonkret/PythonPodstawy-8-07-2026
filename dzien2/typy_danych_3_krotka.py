1f91e9cdd37afa0e842d273a5f3d45fc4759cd90
rajkonkret <rajkonkret660@gmail.com>
2026-07-09 10:05:34 +0200

    [2 4] add typy_danych_3_krotka.py

---
+++ dzien2/typy_danych_3_krotka.py
@@ -0,0 +1,5 @@
+# krotka - kolekcja niemutowalna (tylko do odczytu)
+# pozwala efektywniej zarządzać pamięcią
+
+tupla_imiona = "Zenek", "Tomek", 'Marek', "Ania"
+print(type(tupla_imiona))  # <class 'tuple'>
