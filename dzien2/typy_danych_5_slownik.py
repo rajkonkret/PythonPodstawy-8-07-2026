da37cb0fae59a4151e18bc7d12f118c0900cba34
rajkonkret <rajkonkret660@gmail.com>
2026-07-09 11:44:35 +0200

    [2 25] add typy_danych_5_slownik.py.py

--- dzien2/typy_danych_5_slownik.py
+++ dzien2/typy_danych_5_slownik.py
@@ -49,4 +49,16 @@ print(dictionary['imie'][::-1])  # ['Magda', 'Tomek', 'Radek']
 print(dictionary.get("Imie"))  # None
 print(dictionary.get("Imie", "default"))  # default
 
+print(chr(223))  # ß
+print("\u00DF")  # ß
 
+name1 = "GROSS"
+name2 = "groß"
+
+print(name1.lower() == name2.lower())  # False
+
+"""Casefolding is similar to lowercasing but more aggressive because
+ it is intended to remove all case distinctions in a string.
+  For example, the German lowercase letter 'ß' is equivalent to "ss".
+ Since it is already lowercase, lower() would do nothing to 'ß'; casefold() converts it to "ss"."""
+print(name1.casefold() == name2.casefold())  # True
