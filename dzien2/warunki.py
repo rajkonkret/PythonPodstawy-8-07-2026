39138b23fd0b01efaf9ac5fe5d345a082afecaca
rajkonkret <rajkonkret660@gmail.com>
2026-07-09 12:33:32 +0200

    [2 32] add warunki.py

---
+++ dzien2/warunki.py
@@ -0,0 +1,12 @@
+# instrukcja warunkowa
+# instrukcje sterowanie przepływem programu
+# if
+# w zależności od warunku wykona jeden lub drugi blok programu
+
+odp = True
+
+if odp: print("Test")  # Test
+
+if odp:
+    # blok programu wykonany gdy warunek True
+    print("Test")  # Test
