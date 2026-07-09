025473c6aa346cb419d51e67c321b46fb81f4ea8
rajkonkret <rajkonkret660@gmail.com>
2026-07-09 10:07:25 +0200

    [2 5] add typy_danych_3_krotka.py

--- dzien2/typy_danych_3_krotka.py
+++ dzien2/typy_danych_3_krotka.py
@@ -3,3 +3,9 @@
 
 tupla_imiona = "Zenek", "Tomek", 'Marek', "Ania"
 print(type(tupla_imiona))  # <class 'tuple'>
+
+# tupla_liczby = (43, 45, 22.34, 11, 200)
+tupla_liczby = 43, 45, 22.34, 11, 200
+print(type(tupla_liczby))  # <class 'tuple'>
+print(tupla_liczby)  # (43, 45, 22.34, 11, 200)
+
