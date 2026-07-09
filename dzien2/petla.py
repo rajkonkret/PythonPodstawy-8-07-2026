8a3ab51d53f3b1b5829e652016ee3cefe22ebdeb
rajkonkret <rajkonkret660@gmail.com>
2026-07-09 15:34:51 +0200

    [2 60] add warunki2.py

--- dzien2/petla.py
+++ dzien2/petla.py
@@ -19,3 +19,20 @@ for _ in range(15):
 
 # prawidłowa nazwa zmiennej
 print(_)  # 14
+
+for i in range(5):
+    print(i * 2)
+    print(i + 2)
+# 8
+# 6
+print("Wyjscie z pętli")  # Wyjscie z pętli
+print()
+
+for i in range(10):
+    if i % 2 == 0:  # modulo, reszta z dzielenia
+        print(i, "parzysta")
+# 0 parzysta
+# 2 parzysta
+# 4 parzysta
+# 6 parzysta
+# 8 parzysta
