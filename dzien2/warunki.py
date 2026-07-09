b0ad5998ab8ce9dcd7b08abfc63d603f3a2f172b
rajkonkret <rajkonkret660@gmail.com>
2026-07-09 12:40:25 +0200

    [2 33] add warunki.py

--- dzien2/warunki.py
+++ dzien2/warunki.py
@@ -10,3 +10,27 @@ if odp: print("Test")  # Test
 if odp:
     # blok programu wykonany gdy warunek True
     print("Test")  # Test
+
+# debugger - pozwala wykonac krok po kroku
+# pułapki - miejsce gdzie program się zatrzyma
+
+odp = False
+
+if odp:
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+
+print("Dalsza częśc programu")
+
+odp = "Radek"
+
+if odp:
+    print("Dane zostały wczytane")
+# Dane zostały wczytane
+
+if odp == "Radek":
+    print("Jestem Radek")  # Jestem Radek
