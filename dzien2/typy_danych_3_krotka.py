04c472c33ab56d4148544c20d17699262bf0582f
rajkonkret <rajkonkret660@gmail.com>
2026-07-09 10:15:35 +0200

    [2 7] add typy_danych_3_krotka.py

--- dzien2/typy_danych_3_krotka.py
+++ dzien2/typy_danych_3_krotka.py
@@ -13,3 +13,18 @@ print(tupla_liczby)  # (43, 45, 22.34, 11, 200)
 tupla_jeden = (45,)  # pep8 zaleca nawias
 print(tupla_jeden)  # (45,)
 print(type(tupla_jeden))  # <class 'tuple'>
+
+# tupla_liczby[0] = 123
+# TypeError: 'tuple' object does not support item assignment
+
+# usunięcie całej krotki
+del tupla_liczby
+# print(tupla_liczby)  # NameError: name 'tupla_liczby' is not defined
+
+print(tupla_imiona)
+print(tupla_imiona.index("Zenek"))  # indeks 0
+print(tupla_imiona.count("Zenek"))  # występuje 1 raz
+
+print(len(tupla_imiona))  # liczba elementów: 4
+
+
