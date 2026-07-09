2b622ea7ec4ad5394b70630d49377712df303134
rajkonkret <rajkonkret660@gmail.com>
2026-07-09 11:25:18 +0200

    [2 21] add typy_danych_4_zbior.py

--- dzien2/typy_danych_5_slownik.py
+++ dzien2/typy_danych_5_slownik.py
@@ -12,3 +12,18 @@ print(type(dictionary))  # <class 'dict'>
 dictionary_1 = dict()
 print(dictionary_1)  # {}
 print(type(dictionary_1))  # <class 'dict'>
+
+# dodanie elementów do słownika
+dictionary['imie'] = "Radek"
+print(dictionary)  # {'imie': 'Radek'}
+
+# dodac klucz 'wiek'
+dictionary['wiek'] = 51
+print(dictionary)  # {'imie': 'Radek', 'wiek': 51}
+
+print(dictionary.keys())
+print(dictionary.values())
+print(dictionary.items())
+# dict_keys(['imie', 'wiek'])
+# dict_values(['Radek', 51])
+# dict_items([('imie', 'Radek'), ('wiek', 51)])
