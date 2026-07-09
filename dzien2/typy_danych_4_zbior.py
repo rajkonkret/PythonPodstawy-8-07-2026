f36db41330e8c64e1ade741d6eb4215f1c470979
rajkonkret <rajkonkret660@gmail.com>
2026-07-09 10:40:06 +0200

    [2 12] add typy_danych_4_zbior.py

--- dzien2/typy_danych_4_zbior.py
+++ dzien2/typy_danych_4_zbior.py
@@ -1,3 +1,8 @@
 # zbior (set) - przechowuje unikalne wartości
 # nie zachowuje kolejnosci
 # nie posiada indeksu
+
+lista = [44, 55, 66, 777, 33, 22, 11, 33, 11, 777]
+zbior = set(lista)  # rzutowanie na zbiór
+print(zbior)  # {33, 66, 777, 11, 44, 22, 55}
+print(type(zbior))  # <class 'set'>
