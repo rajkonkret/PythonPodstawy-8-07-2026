cfc736bca163c3374a3e05a7ed21de1be87d4cf0
rajkonkret <rajkonkret660@gmail.com>
2026-07-09 15:31:51 +0200

    [2 59] add warunki2.py

---
+++ dzien2/petla.py
@@ -0,0 +1,21 @@
+# pętla - mozliwosc wykonania kodu wielokrotnie
+# for - pętla iteracyjna
+
+for i in range(5):  # od 0 do 4
+    print(i)
+# 0
+# 1
+# 2
+# 3
+# 4
+
+for i in range(30):
+    pass  # nic nie rób
+
+print(i)  # 29
+
+for _ in range(15):
+    print("Test podłoga")
+
+# prawidłowa nazwa zmiennej
+print(_)  # 14
