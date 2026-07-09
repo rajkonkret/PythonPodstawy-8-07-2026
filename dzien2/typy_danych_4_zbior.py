bdec6e56fe61045de5349e8ac67398c9a21550b1
rajkonkret <rajkonkret660@gmail.com>
2026-07-09 10:42:22 +0200

    [2 14] add typy_danych_4_zbior.py

--- dzien2/typy_danych_4_zbior.py
+++ dzien2/typy_danych_4_zbior.py
@@ -6,3 +6,23 @@ lista = [44, 55, 66, 777, 33, 22, 11, 33, 11, 777]
 zbior = set(lista)  # rzutowanie na zbiór
 print(zbior)  # {33, 66, 777, 11, 44, 22, 55}
 print(type(zbior))  # <class 'set'>
+
+# pusty zbiór
+zb2 = set()  # tylko słowko set
+print(zb2)  # set()
+
+# dodanie elementu do zbioru
+zbior.add(33)
+zbior.add(33)
+zbior.add(33)
+zbior.add(33)
+zbior.add(18)
+zbior.add(33)
+zbior.add(18)
+zbior.add(33)
+zbior.add(18)
+zbior.add(18)
+zbior.add(25)
+zbior.add(33)
+
+print(zbior) # {33, 66, 777, 11, 44, 18, 22, 55, 25}
