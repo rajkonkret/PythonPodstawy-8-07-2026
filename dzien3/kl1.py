b91dfb4dfaa1f3b9c97a7e9da84f1e99c6e00f97
rajkonkret <rajkonkret660@gmail.com>
2026-07-10 12:43:12 +0200

    [3 29] add fun1.py

---
+++ dzien3/kl1.py
@@ -0,0 +1,15 @@
+# klasa -  przepis
+#  obiekt - zbudowany wg klasy
+# hermetyzacja, dziedziczenie , polimorfizm i abstrakcja
+
+# CamelCase
+class Human:
+    """
+    Klasa Human
+    """
+
+
+print(Human.__doc__)  # Klasa Human
+# print(print.__doc__)
+# pydoc -b
+# pydoc -w kl1
