3b3b4c30429db3d536be3fe4b82cafb7c8bd617d
rajkonkret <rajkonkret660@gmail.com>
2026-07-10 14:06:55 +0200

    [3 35] add api_nbp.py

---
+++ dzien3/api_nbp.py
@@ -0,0 +1,9 @@
+# REST API (ang. Representational State Transfer) to powszechny styl architektury oprogramowania,
+# który pozwala aplikacjom komunikować się ze sobą przez protokół HTTP
+# metody http
+# GET: Pobieranie danych z serwera. Jest bezpieczna i idempotentna
+# POST: Przesyłanie nowych danych do serwera (np. zawartości formularza), często w celu utworzenia nowego zasobu.
+# PUT: Aktualizacja lub zastąpienie wskazanego zasobu nowymi danymi.
+# PATCH: Częściowa modyfikacja zasobu.
+# DELETE: Żądanie usunięcia określonego zasobu.
+url = "https://api.nbp.pl/api/exchangerates/rates/A/eur/"
