daaf868b0b67237b964eede0ab601091f2084246
rajkonkret <rajkonkret660@gmail.com>
2026-07-10 12:44:52 +0200

    [3 30 add fun1.py

--- dzien3/kl1.py
+++ dzien3/kl1.py
@@ -8,6 +8,17 @@ class Human:
     Klasa Human
     """
 
+    def __init__(self, imie, wiek, plec="k"):
+        """
+        Metoda inicjalizująca
+        :param imie:
+        :param wiek:
+        :param plec:
+        """
+        self.imie = imie
+        self.wiek = wiek
+        self.plec = plec
+
 
 print(Human.__doc__)  # Klasa Human
 # print(print.__doc__)
