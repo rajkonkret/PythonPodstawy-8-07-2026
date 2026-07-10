518b07f51f50f65e0f64a853d75059acc00129b5
rajkonkret <rajkonkret660@gmail.com>
2026-07-10 15:08:24 +0200

    [3 41] add baza

---
+++ dzien3/baza.py
@@ -0,0 +1,18 @@
+# baza danych - silnik
+# relacyjne i nieralcyjne
+# sql
+# sqlite
+
+import sqlite3
+
+try:
+    conn = sqlite3.connect('baza_danych.db')
+
+    c = conn.cursor()
+    print('Baza danych zostala podłaczona')
+except sqlite3.Error as e:
+    print("Bład:", e)
+finally:
+    if conn:
+        conn.close()
+        print("Połaczenie zostało zamknięte")
