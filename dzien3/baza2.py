e40ea4cd6c8cbbae3f33974e044f46961cb9a7d8
rajkonkret <rajkonkret660@gmail.com>
2026-07-10 15:19:46 +0200

    [3 45] add baza

--- dzien3/baza2.py
+++ dzien3/baza2.py
@@ -12,18 +12,33 @@ try:
     print('Baza danych zostala podłaczona')
 
     query = """
-    CREATE TABLE IF NOT EXISTS developers (
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    salary REAL NOT NULL);
+            CREATE TABLE IF NOT EXISTS developers
+            (
+                id
+                INTEGER
+                PRIMARY
+                KEY,
+                name
+                TEXT
+                NOT
+                NULL,
+                salary
+                REAL
+                NOT
+                NULL
+            );
             """
 
     c.execute(query)
     conn.commit()
 
-    insert = "INSERT INTO developers (id,name,salary) VALUES (2, 'Radek', 12000);"
-    c.execute(insert)
-    conn.commit()
+    # insert = "INSERT INTO developers (id,name,salary) VALUES (2, 'Radek', 12000);"
+    # c.execute(insert)
+    # conn.commit()
+
+    select = "SELECT * FROM developers"
+    for row in c.execute(select):
+        print(row) # (2, 'Radek', 12000.0)
 
 except sqlite3.Error as e:
     print("Bład:", e)
