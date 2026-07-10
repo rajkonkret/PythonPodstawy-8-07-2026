703ad83a1bce78b72135465075c6b9f099539b82
rajkonkret <rajkonkret660@gmail.com>
2026-07-10 10:05:51 +0200

    [3 8] add random_zad1.py

--- dzien3/random_zad1.py
+++ dzien3/random_zad1.py
@@ -8,3 +8,7 @@ print(random.randrange(5))  # od 0 do 4
 
 print(random.random())  # 0.9491859510930893 od 0 do 0.99999999
 print(random.random() * 9)  # 7.323289348995436 od 0 do 8.99999999
+
+lista = [67, 45, 32, 68, 90, 42, 49]
+# ctrl alt l
+print(lista[random.randrange(len(lista))])  # 45
