89554c46af0929f8416893f9b76cd810a6ff26db
rajkonkret <rajkonkret660@gmail.com>
2026-07-10 11:00:07 +0200

    [3 18] add pliki_zad1.py

--- dzien3/pliki_zad1.py
+++ dzien3/pliki_zad1.py
@@ -5,20 +5,26 @@
 # with - context manager w pythonie
 
 # https://docs.python.org/3.14/library/functions.html#open
-with open('test.log', "w") as file:
+with open('test.log', "w", encoding='utf-8') as file:
     file.write("Powitanie\n")
     file.write("Jedno jeszcze\n")
 
 # FileExistsError: [Errno 17] File exists: 'test.log'
-# with open('test.log', "x") as file:
+# with open('test.log', "x",  encoding='utf-8') as file:
 #     file.write("Powitanie\n")
 #     file.write("Jedno jeszcze\n")
 
-with open('test.log', "w") as file:
+with open('test.log', "w", encoding='utf-8') as file:
     file.write("Powitanie\n")
     file.write("Jedno jeszcze\n")
 
 # dopisanie do pliku, na końcu
-with open('test.log', "a") as file:
+with open('test.log', "a", encoding='utf-8') as file:
     file.write("Dopisane\n")
     file.write("Dopisane 2\n")
+    file.write("Dośdane\n")
+
+with open('test.log', "r", encoding='utf-8') as fh:
+    lines = fh.read()
+
+print(lines)
