219d1e632e6137c552fef5179baf1da83a820e62
rajkonkret <rajkonkret660@gmail.com>
2026-07-10 10:15:47 +0200

    [3 9] add random_zad1.py

--- dzien3/random_zad1.py
+++ dzien3/random_zad1.py
@@ -1,4 +1,5 @@
 import random
+from random import randrange
 
 """Return random integer in range [a, b], including both end points."""
 print(random.randint(1, 100))  # od 1 do 100
@@ -12,3 +13,23 @@ print(random.random() * 9)  # 7.323289348995436 od 0 do 8.99999999
 lista = [67, 45, 32, 68, 90, 42, 49]
 # ctrl alt l
 print(lista[random.randrange(len(lista))])  # 45
+
+print(random.choice(lista))  # losuje element z kolekcji, 90
+
+lista_kul = list(range(1, 50))  # od 1 do 49
+
+for _ in range(6):  # od 0 do 5
+    kula = random.choice(lista_kul)
+    lista_kul.remove(kula)
+    print(kula)
+# 26
+# 35
+# 4
+# 38
+# 29
+# 6
+
+print(random.choices(lista_kul, k=6))  # [34, 17, 20, 2, 30, 2] z powtórzeniami
+
+print(random.sample(lista_kul, k=6))  # [30, 11, 24, 5, 4, 31], bez powtórzen
+print(sorted(random.sample(lista_kul, k=6)))  # [4, 13, 27, 33, 41, 46]
