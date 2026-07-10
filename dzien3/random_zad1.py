4464e24d447a4ab0250ed83275d567b2944444e1
rajkonkret <rajkonkret660@gmail.com>
2026-07-10 10:03:23 +0200

    [3 7] add random_zad1.py

---
+++ dzien3/random_zad1.py
@@ -0,0 +1,10 @@
+import random
+
+"""Return random integer in range [a, b], including both end points."""
+print(random.randint(1, 100))  # od 1 do 100
+
+print(random.randrange(1, 100))  # od 1 do 99
+print(random.randrange(5))  # od 0 do 4
+
+print(random.random())  # 0.9491859510930893 od 0 do 0.99999999
+print(random.random() * 9)  # 7.323289348995436 od 0 do 8.99999999
