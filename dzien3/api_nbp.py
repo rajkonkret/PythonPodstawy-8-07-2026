311faba83279825ef7ccc3f638e1e43c6d04d679
rajkonkret <rajkonkret660@gmail.com>
2026-07-10 14:17:10 +0200

    [3 36] add api_nbp.py

--- dzien3/api_nbp.py
+++ dzien3/api_nbp.py
@@ -6,4 +6,37 @@
 # PUT: Aktualizacja lub zastąpienie wskazanego zasobu nowymi danymi.
 # PATCH: Częściowa modyfikacja zasobu.
 # DELETE: Żądanie usunięcia określonego zasobu.
+# https://github.com/public-apis/public-apis
+
+import requests
+
+# pip install requests
+
 url = "https://api.nbp.pl/api/exchangerates/rates/A/eur/"
+
+response = requests.get(url)
+print(response)  # <Response [200]>
+# 200 ok
+# 3xx - warningi, przekierowania
+# 4xx - 404 brak strony, 400 Bad Request  - błedne dane
+# 5xx - 500 internal Server Error
+
+print(response.text) # str
+
+dane = response.json()
+print(dane)
+# {'table': 'A',
+# 'currency': 'euro',
+# 'code': 'EUR',
+# 'rates': [{'no': '132/A/NBP/2026', 'effectiveDate': '2026-07-10', 'mid': 4.3465}]}
+#
+print(type(dane)) # <class 'dict'>
+
+for k in dane:
+    print(k)
+# table
+# currency
+# code
+# rates
+
+# waluta, kurs
